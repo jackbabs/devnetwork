88b453be62009988528e4f4a21c3957681a3238c
jackbaber <thejackbabs@gmail.com>
2018-10-25 10:51:02 +0100

    initial express server with route files

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb://roger:roger66@ds239903.mlab.com:39903/devnetwork"
+};
---
+++ routes/api/profile.js
@@ -0,0 +1,8 @@
+// for user info
+
+const express = require("express");
+const router = express.Router();
+
+router.get("/test", (req, res) => res.json({ msg: "profile works" }));
+
+module.exports = router;
